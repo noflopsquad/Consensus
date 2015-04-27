26cb93bce2bd7227837f7da0ca43cdabaa536748
xaviuzz <xaviuzz@gmail.com>
2015-04-27 15:28:30 +0200

    Redo Specs with proper context strategy

--- spec/consensus_spec.rb
+++ spec/consensus_spec.rb
@@ -3,60 +3,65 @@ require './lib/consensus'
 require './lib/question'
 
 describe 'A Consensus' do
-  let(:proposal) { Proposal.new(:proposer) }
-  let(:consensus) { Consensus.new(proposal) }
-  let(:clarifying_question) { Question.new(:questioner) }
+  
+  before do
+    @proposal = Proposal.new(:proposer)
+    @consensus= Consensus.new(@proposal)
+    @clarifying_question = Question.new(:questioner)
+  end
 
   it 'needs a proposal' do
     non_proposal = nil
 
     expect{Consensus.new(non_proposal)}.to raise_error "needs a proposal"
-    expect{Consensus.new(proposal)}.to_not raise_error
+    expect{Consensus.new(@proposal)}.to_not raise_error
   end
 
   it 'starts with an introduction' do
-    expect(consensus.status).to eq 'Introduction'
+    expect(@consensus.status).to eq 'Introduction'
   end
 
   it 'has the proposer as a participant' do
-    expect(consensus.is_involved? :proposer).to eq true
-    expect(consensus.is_involved? :not_the_proposer).to eq false
+    expect(@consensus.is_involved? :proposer).to eq true
+    expect(@consensus.is_involved? :not_the_proposer).to eq false
   end
 
   it "accepts question in the introduccion phase" do
-    consensus.address(clarifying_question)
-    expect(consensus.any_questions?).to eq true
+    @consensus.address(@clarifying_question)
+    expect(@consensus.any_questions?).to eq true
   end
 
-  it "considers the questioner involved" do
-    expect(consensus.is_involved? :questioner).to eq false
-    consensus.address(clarifying_question)
-    expect(consensus.is_involved? :questioner).to eq true
-  end
+  describe 'when a clarifying question is addressed' do
+    before do
+      @consensus.address(@clarifying_question)
+    end
 
-  it "address clarifying questions to the proposer" do
-    consensus.address(clarifying_question)
-    expect(clarifying_question.whom).to eq :proposer
-  end
+    it "considers the questioner involved" do
+      expect(@consensus.is_involved? :questioner).to eq true
+    end
 
-  it "considers open any unaccepted question" do
-    consensus.address(clarifying_question)
-    clarifying_question.answer :proposer
-    expect(consensus.any_questions?).to eq true
-  end
+    it "questions are addressed to the proposer" do
+      expect(@clarifying_question.whom).to eq :proposer
+    end
 
-  it "considers closed accepted questions" do
-    consensus.address(clarifying_question)
-    clarifying_question.answer :proposer
-    clarifying_question.accept :questioner
-    expect(consensus.any_questions?).to eq false
+    it "considers open any unaccepted question" do
+      @clarifying_question.answer :proposer
+      expect(@consensus.any_questions?).to eq true
+    end
+
+    it "considers closed accepted questions" do
+      @clarifying_question.answer :proposer
+      @clarifying_question.accept :questioner
+      expect(@consensus.any_questions?).to eq false
+    end
+
   end
 
   it "the introduction phase has a minimum duration " do
     minimum_duration_in_days = 2
-    expect{consensus.next_phase}.to raise_error "Minimum duration not reached yet"
+    expect{@consensus.next_phase}.to raise_error "Minimum duration not reached yet"
     time_passes minimum_duration_in_days + 1
-    expect{consensus.next_phase}.not_to raise_error
+    expect{@consensus.next_phase}.not_to raise_error
   end
 
   def time_passes days
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -1,73 +1,79 @@
 describe 'A Question' do
-  let(:question) { Question.new(:questioner) }
-  
+
+  before do
+    @question = Question.new(:questioner)
+  end
+
   it 'starts unanswered' do
-    expect(question.answered?).to eq false
+    expect(@question.answered?).to eq false
   end
 
   it 'can be answered' do
-    question.answer :any_person
-    expect(question.answered?).to eq true
+    @question.answer :any_person
+    expect(@question.answered?).to eq true
   end
 
   it "can be addressed" do
-    question.address :person
-    expect(question.whom).to eq :person
+    @question.address :person
+    expect(@question.whom).to eq :person
   end
 
 
   describe 'when addressed' do
     it "only could be answered by the addressed person" do
-      question.address :person
-      expect{question.answer(:other_than_addressed)}.to raise_error 'The question is addressed to another person'
+      @question.address :person
+      expect{@question.answer(:other_than_addressed)}.to raise_error 'The question is addressed to another person'
     end
   end
 
   it "must be answered to be accepted " do
-      expect{question.accept :questioner}.to raise_error 'must be answered before' 
+      expect{@question.accept :questioner}.to raise_error 'must be answered before' 
   end
 
   it "must be answered to be rejected " do
-      expect{question.reject :questioner, :any_reason}.to raise_error 'must be answered before' 
+      expect{@question.reject :questioner, :any_reason}.to raise_error 'must be answered before' 
   end
 
   describe 'when answered' do
-    let(:answered) {question.answer :anyone
-                            question}
+
+    before do
+      @question.answer :anyone
+    end
     
     it "can be accepted by the questioner" do
-      answered.accept :questioner
-      expect(question.accepted?).to eq true
+      @question.accept :questioner
+      expect(@question.accepted?).to eq true
     end
 
     it "must be accepted by the questioner" do
-      expect{answered.accept :anybody}.to raise_error 'this action must be made by the questioner'
+      expect{@question.accept :anybody}.to raise_error 'this action must be made by the questioner'
     end
 
     it "can be rejected with a reason" do
-      answered.reject :questioner,:any_reason
-      expect(answered.rejected?).to eq true  
+      @question.reject :questioner,:any_reason
+      expect(@question.rejected?).to eq true  
     end
 
     it "must be rejected by the questioner" do
-      expect{answered.reject :anybody, :any_reason}.to raise_error 'this action must be made by the questioner'
+      expect{@question.reject :anybody, :any_reason}.to raise_error 'this action must be made by the questioner'
     end
 
   end
 
   describe 'when rejected' do
-    let(:rejected) {question.answer :anyone
-                    question.reject :questioner,:any_reason
-                    question}
-    
+    before do
+      @question.answer :anyone
+      @question.reject :questioner,:any_reason
+    end
+
     it "can be answered again" do
-      rejected.answer :anybody
-      expect(rejected.answered?).to eq true
+      @question.answer :anybody
+      expect(@question.answered?).to eq true
     end
 
     it "can be accepted" do
-      rejected.accept :questioner
-      expect(rejected.accepted?).to eq true
+      @question.accept :questioner
+      expect(@question.accepted?).to eq true
     end
 
   end
